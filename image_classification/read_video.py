cf4b36199574311f2ef4c676669ec36291d92d8e
tngph.m <tungpv2902@gmail.com>
2025-10-17 07:07:20 +0700

    image classification dataloader

---
+++ image_classification/read_video.py
@@ -0,0 +1,55 @@
+import os
+import cv2
+from tqdm.auto import tqdm
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader
+
+
+def _doc_video_image_folder(input_folder: str, output_folder: str):
+    # Tạo thư mục output nếu chưa tồn tại
+    os.makedirs(output_folder, exist_ok=True)
+
+    for root, dirs, files in tqdm(os.walk(input_folder), desc="Processing folders"):
+        for file in tqdm(files, desc="Processing files", leave=False):
+            if file.lower().endswith(('.jpg', '.jpeg', '.png')):
+                file_path = os.path.join(root, file)
+                image = cv2.imread(file_path)
+
+                if image is None:
+                    continue
+
+                if os.path.abspath(root).lower().count("live") != 0:
+                    img_class = "live"
+                else:
+                    img_class = "spoof"
+                output_subfolder = os.path.join(output_folder, img_class)
+                os.makedirs(output_subfolder, exist_ok=True)
+
+                output_path = f"{os.path.join(output_subfolder, file)}.jpg"
+                cv2.imwrite(output_path, image)
+            if file.lower().endswith(('.mov', '.mp4', '.avi', '.mkv')):
+                cap = cv2.VideoCapture(os.path.join(root, file))
+                if not cap.isOpened():
+                    continue
+                total_frame = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+                cap.set(cv2.CAP_PROP_POS_FRAMES, total_frame//2)
+                flag, frame = cap.read()
+                if flag:
+                    if os.path.abspath(root).lower().count("live") != 0:
+                        img_class = "live"
+                    else:
+                        img_class = "spoof"
+                    output_subfolder = os.path.join(output_folder, img_class)
+                    print(output_subfolder)
+                    os.makedirs(output_subfolder, exist_ok=True)
+
+                    output_path = f"{os.path.join(output_subfolder, file)}.jpg"
+                    cv2.imwrite(output_path, frame)
+                cap.release()
+
+
+_doc_video_image_folder(r"E:\Luyen-AIO\test_image", r"E:\Luyen-AIO\hola")
+
+dataset = ImageFolder(r"E:\Luyen-AIO\hola")
+dataloader = DataLoader(dataset=dataset, batch_size=64, num_workers=4)
+print(dataloader)
